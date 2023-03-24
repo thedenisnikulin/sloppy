2fa026d0bb6313b949e1537911d98bb0a999e4de
Denis Nikulin <thedenisnikulin@gmail.com>
2023-03-24 22:18:35 +0300

    chore: change test cmd

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::{env, process::Command};
 fn main() {
     env::set_var("LD_PRELOAD", "./sloppy/target/debug/libsloppy.so");
 
-    let out = Command::new("firefox")
+    let out = Command::new("ping")
+        .arg("google.com")
         .spawn()
         .expect("could not spawn cmd");
 
